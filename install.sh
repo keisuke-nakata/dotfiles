e27a928c3dd97c3b2eed96e0d0b66d3f4f125cc3
NAKATA Keisuke <keisuke.nakata@brainpad.co.jp>
2018-03-09 20:22:24 +0900

    add imgcat

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for d in ".tmux.conf"; do
   ln -snfv ${HERE}/$d ${HOME}/$d
 done
+
+mkdir $HOME/bin
+for c in "imgcat"; do
+  ln -snfv ${HERE}/$c ${HOME}/bin/$c
+done
