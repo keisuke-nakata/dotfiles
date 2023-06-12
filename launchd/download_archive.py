675dfd3c945e952a17b3efe22255e4afc48af814
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-06-13 08:57:05 +0900

    fix

--- launchd/download_archive.py
+++ launchd/download_archive.py
@@ -5,35 +5,37 @@ from pathlib import Path
 import shutil
 import sys
 
+PROGRAM_NAME = "DownloadArchive"
+DIR = Path("/Users/keisuke.nakata/Downloads")
+
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 formatter = logging.Formatter(fmt)
 ch.setFormatter(formatter)
 
-logger = logging.getLogger("DownloadArchive")
+logger = logging.getLogger(PROGRAM_NAME)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 dirname = dt.datetime.now().strftime("%Y%m%d")
-downloads_dir = Path("/Users/keisuke.nakata/Downloads")
-archive_dir = downloads_dir / dirname
+archive_dir = DIR / dirname
 if archive_dir.exists():
-    logger.warning(f"{dirname} already exists. Skipped DownloadArchive.")
+    logger.warning(f"{dirname} already exists. Skipped {PROGRAM_NAME}.")
     sys.exit()
-logger.debug("Archiving Downloads...")
+logger.debug(f"{PROGRAM_NAME}...")
 
 yyyymmdd_pat = re.compile(r"^202\d(0[1-9]|10|11|12)([012]\d|30|31)$")
 moving_targets = [
-    f for f in downloads_dir.iterdir()
+    f for f in DIR.iterdir()
     if not (f.is_dir() and re.match(yyyymmdd_pat, f.name) is not None)
 ]
 
 if len(moving_targets) == 0:
-    logger.debug("There is no new Downloads.")
+    logger.debug("There is no new file/directory to archive.")
     sys.exit()
 
 archive_dir.mkdir(exist_ok=False)
 for moving_target in moving_targets:
     shutil.move(moving_target, archive_dir)
-logger.info("Archiving Downloads... done.")
+logger.info(f"{PROGRAM_NAME}... done.")
